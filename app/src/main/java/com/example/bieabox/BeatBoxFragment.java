1ee23f19e70fa4806ab966dde5cfc71b17e11ae1
couchjanus <couchjanus@gmail.com>
2021-11-11 16:04:11 +0200

    first commit

---
+++ app/src/main/java/com/example/bieabox/BeatBoxFragment.java
@@ -0,0 +1,93 @@
+package com.example.bieabox;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+import com.example.bieabox.databinding.FragmentBeatBoxBinding;
+//import com.example.bieabox.databinding.ListItemSoundBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BeatBoxFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BeatBoxFragment extends Fragment {
+
+    private BeatBox mBeatBox;
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
+    }
+
+    @Override
+    public  View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                                      Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_beat_box, container, false);
+
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+//        binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
+
+        return binding.getRoot();
+    }
+
+//    private class SoundHolder extends RecyclerView.ViewHolder {
+//        private ListItemSoundBinding mBinding;
+//
+//        private SoundHolder(ListItemSoundBinding binding) {
+//            super(binding.getRoot());
+//            mBinding = binding;
+//            mBinding.setViewModel(new SoundViewModel(mBeatBox));
+//        }
+//
+//        public void bind(Sound sound) {
+//            mBinding.getViewModel().setSound(sound);
+//            mBinding.executePendingBindings();
+//        }
+//    }
+
+//    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+//        private List<Sound> mSounds;
+//
+//        public SoundAdapter(List<Sound> sounds) {
+//            mSounds = sounds;
+//        }
+//
+//        @Override
+//        public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            LayoutInflater inflater = LayoutInflater.from(getActivity());
+//            ListItemSoundBinding binding = DataBindingUtil
+//                    .inflate(inflater, R.layout.list_item_sound, parent, false);
+//            return new SoundHolder(binding);
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(SoundHolder holder, int position) {
+//            Sound sound = mSounds.get(position);
+//            holder.bind(sound);
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return mSounds.size();
+//        }
+//    }
+}
---
+++ app/src/main/java/com/example/bieabox/SingleFragmentActivity.java
@@ -0,0 +1,28 @@
+package com.example.bieabox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = createFragment();
+            manager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
